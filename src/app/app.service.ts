28483edcad23582910342ebabc5cfbafb335c018
Gyuzal <aidarkhanova.g.03@mail.ru>
2023-06-19 13:39:53 +0200

    l

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -9,11 +9,11 @@ export class AppService {
   constructor( private http: HttpClient) { }
 
   sendOrder(data: any){
-    return this.http.post('https://testologia.site/burgers-order', data)
+    return this.http.post('https://testologia.site/burgers-order', data);
   }
 
   
   getData(){
-    return this.http.get('https://testologia.site/burgers-data?extra=black')
+    return this.http.get('https://testologia.site/burgers-data?extra=black');
   }
 }
